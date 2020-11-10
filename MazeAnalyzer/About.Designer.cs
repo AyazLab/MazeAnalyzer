e0e01a2f2c00356b26dca0055327643f9c7c26cf
Adrian Curtin <adrian.b.curtin@gmail.com>
2020-11-10 08:46:16 -0500

    Update About.Designer.cs
    Updated credits and copyright year

--- MazeAnalyzer/About.Designer.cs
+++ MazeAnalyzer/About.Designer.cs
@@ -42,6 +42,7 @@ namespace MazeAnalyzer
             this.label2 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
             pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -53,10 +54,10 @@ namespace MazeAnalyzer
             pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             pictureBox1.Image = global::MazeAnalyzer.Properties.Resources.mazeAnalyzer_500;
             pictureBox1.InitialImage = null;
-            pictureBox1.Location = new System.Drawing.Point(6, 8);
-            pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            pictureBox1.Location = new System.Drawing.Point(7, 10);
+            pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new System.Drawing.Size(192, 203);
+            pictureBox1.Size = new System.Drawing.Size(216, 253);
             pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
@@ -66,20 +67,20 @@ namespace MazeAnalyzer
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Maroon;
-            this.label1.Location = new System.Drawing.Point(210, 8);
+            this.label1.Location = new System.Drawing.Point(236, 10);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(216, 36);
+            this.label1.Size = new System.Drawing.Size(257, 40);
             this.label1.TabIndex = 0;
             this.label1.Text = "MazeAnalyzer";
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(406, 321);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(457, 401);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(112, 35);
             this.button1.TabIndex = 3;
             this.button1.Text = "Close";
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -87,40 +88,40 @@ namespace MazeAnalyzer
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(213, 110);
+            this.label3.Location = new System.Drawing.Point(240, 138);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(198, 17);
+            this.label3.Size = new System.Drawing.Size(222, 20);
             this.label3.TabIndex = 4;
             this.label3.Text = "Hasan Ayaz and Adrian Curtin";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(213, 82);
+            this.label4.Location = new System.Drawing.Point(240, 102);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(125, 17);
+            this.label4.Size = new System.Drawing.Size(138, 20);
             this.label4.TabIndex = 5;
-            this.label4.Text = "Copyright (c) 2018";
+            this.label4.Text = "Copyright (c) 2020";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(213, 53);
+            this.label6.Location = new System.Drawing.Point(240, 66);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(60, 17);
+            this.label6.Size = new System.Drawing.Size(67, 20);
             this.label6.TabIndex = 7;
             this.label6.Text = "Version ";
             // 
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(296, 321);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(333, 401);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.Size = new System.Drawing.Size(112, 35);
             this.button2.TabIndex = 8;
             this.button2.Text = "Update";
             this.button2.UseVisualStyleBackColor = true;
@@ -131,10 +132,10 @@ namespace MazeAnalyzer
             // 
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel1.Location = new System.Drawing.Point(213, 135);
+            this.linkLabel1.Location = new System.Drawing.Point(240, 169);
             this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(205, 20);
+            this.linkLabel1.Size = new System.Drawing.Size(240, 25);
             this.linkLabel1.TabIndex = 9;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "http://www.mazesuite.com";
@@ -142,11 +143,12 @@ namespace MazeAnalyzer
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 253);
+            this.textBox1.Location = new System.Drawing.Point(15, 316);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(490, 62);
+            this.textBox1.Size = new System.Drawing.Size(551, 76);
             this.textBox1.TabIndex = 10;
             this.textBox1.Text = resources.GetString("textBox1.Text");
             // 
@@ -154,9 +156,9 @@ namespace MazeAnalyzer
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.Purple;
-            this.label2.Location = new System.Drawing.Point(13, 230);
+            this.label2.Location = new System.Drawing.Point(15, 288);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(490, 17);
+            this.label2.Size = new System.Drawing.Size(546, 20);
             this.label2.TabIndex = 11;
             this.label2.Text = "Please use the following citation to reference MazeSuite in your publications:";
             // 
@@ -165,10 +167,10 @@ namespace MazeAnalyzer
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label5.Location = new System.Drawing.Point(13, 324);
+            this.label5.Location = new System.Drawing.Point(15, 405);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(179, 20);
+            this.label5.Size = new System.Drawing.Size(212, 25);
             this.label5.TabIndex = 6;
             this.label5.Text = "Checking for updates...";
             // 
@@ -177,11 +179,22 @@ namespace MazeAnalyzer
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(239, 219);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(189, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Heatmap by Micheal Tran";
+            // 
             // About
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(515, 361);
+            this.ClientSize = new System.Drawing.Size(579, 451);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.linkLabel1);
@@ -194,7 +207,7 @@ namespace MazeAnalyzer
             this.Controls.Add(pictureBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "About";
@@ -221,5 +234,6 @@ namespace MazeAnalyzer
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label7;
     }
 }
